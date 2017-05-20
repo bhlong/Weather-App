a9d197cdc06ccfe19dfae2d68927552df307f937
Jason <jasonlong98@hotmail.com>
2017-05-20 12:19:21 -0400

    added code for simpleweather.js

---
+++ js/weather.js
@@ -0,0 +1,37 @@
+var x;
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+        x = position.coords.latitude + ',' + position.coords.longitude;
+        loadWeather(x);
+    });
+} else {
+    loadWeather("Waterloo, ON", "");
+}
+
+$(document).ready(function () {
+    setInterval(function() {loadWeather(x);}, 10000);
+});
+
+function loadWeather(location, woeid) {
+    $.simpleWeather({
+        location: location,
+        woeid: woeid,
+        unit: 'c',
+        success: function (weather) {
+            city = weather.city;
+            temp = weather.temp + '&deg;';
+            wcode = '<img class="weathericon" src="images/weathericons/' + weather.code + '.svg">';
+            wind = '<p>' + weather.wind.speed + '</p><p>' + weather.units.speed + '</p>';
+            humidity = weather.humidity + ' %'
+
+            $(".location").text(city);
+            $(".temperature").html(temp);
+            $(".climate_bg").html(wcode);
+            $(".windspeed").html(wind);
+            $(".humidity").text(humidity);
+        },
+        error: function (error) {
+            $(".error").html("<p>" + error + "</p>");
+        }
+    });
+}
